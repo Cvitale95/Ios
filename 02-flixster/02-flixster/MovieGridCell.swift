3e1b814b4961d54bec4579b15be2cf20df823034
Chris <chrisv@Chriss-MacBook-Pro.local>
2019-09-19 12:26:53 -0700

    FlixPartTwo

---
+++ 02-flixster/02-flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  02-flixster
+//
+//  Created by Chris  on 9/18/19.
+//  Copyright © 2019 Chris . All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
